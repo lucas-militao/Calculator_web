60797542d7494f84398e9c690f9fd146aa265284
lumilitas <lucas23121995>
2020-12-15 11:40:22 -0300

    added pad containing buttons

--- src/App.js
+++ src/App.js
@@ -8,6 +8,31 @@ export default function App() {
     <>
       <div id="calculator">
         <input type="number" value={result} disabled="disabled"/>
+
+        <div id="line1">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>+</button>
+        </div>
+        <div id="line2">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div id="line3">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>=</button>
+        </div>
+        <div id="line4">
+          <button>AC</button>
+          <button>0</button>
+          <button>C</button>
+          <button>/</button>
+        </div>
       </div>
     </>
   )
