34f8a87c776e6a8f9ef9ca5131ea2ca8d3da7fdc
lumilitas <lucas23121995>
2020-12-17 08:40:56 -0300

    user input implemented

--- src/App.js
+++ src/App.js
@@ -2,36 +2,49 @@ import React, { useState } from 'react';
 
 export default function App() {
 
-  const [ result, setResult ] = useState(0);
+  const [ result, setResult ] = useState('0');
+  const [ userInput, setUserInput ] = useState('0');
+
+  function handleInput(event) {
+    const digit = event.target.value;
+    
+    if (userInput == '0') {
+      setUserInput(digit)
+    } else if (userInput.length < 8) {
+      setUserInput(userInput + digit);
+    }
+  }
 
   return (
     <>
       <div id="calculator">
-        <input type="number" value={result} disabled="disabled"/>
+        <input type="text" value={userInput} disabled="disabled"/>
 
-        <div id="line1">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>+</button>
-        </div>
-        <div id="line2">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-        </div>
-        <div id="line3">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>=</button>
-        </div>
-        <div id="line4">
-          <button>AC</button>
-          <button>0</button>
-          <button>C</button>
-          <button>/</button>
+        <div id="pad">
+          <div id="line1">
+            <button onClick={handleInput} value="7">7</button>
+            <button onClick={handleInput} value= "8">8</button>
+            <button onClick={handleInput} value= "9">9</button>
+            <button>+</button>
+          </div>
+          <div id="line2">
+            <button onClick={handleInput} value= "4">4</button>
+            <button onClick={handleInput} value= "5">5</button>
+            <button onClick={handleInput} value= "6">6</button>
+            <button>-</button>
+          </div>
+          <div id="line3">
+            <button onClick={handleInput} value= "1">1</button>
+            <button onClick={handleInput} value= "2">2</button>
+            <button onClick={handleInput} value= "3">3</button>
+            <button>=</button>
+          </div>
+          <div id="line4">
+            <button>AC</button>
+            <button>0</button>
+            <button>C</button>
+            <button>/</button>
+          </div>
         </div>
       </div>
     </>
